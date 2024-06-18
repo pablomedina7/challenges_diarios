9ea473735c1043ebddfb73ca785b86d01473ae60
pablomedina7 <pablomedinasantos@hotmail.com>
2024-06-18 10:37:09 -0400

    Create challengedia6.py

---
+++ challengedia6.py
@@ -0,0 +1,6 @@
+#CONVERTIDOR DE TEMPERATURA
+celsius= float(input("introduce la temperatura en grados celsius: "))
+
+fahrenheit= celsius * (9/5) + 32 
+
+print(f"{celsius} grados son {fahrenheit} grados fahrenheit")
